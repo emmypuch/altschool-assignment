7140b644212376b332f8032438405ba1dc5ce3dc
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-12-14 22:53:43 +0100

    modified files

--- nav.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navMenu = document.getElementById("nav-menu");
-
-  menuToggle.addEventListener("click", function () {
-    navMenu.classList.toggle("show-menu");
-  });
-});
